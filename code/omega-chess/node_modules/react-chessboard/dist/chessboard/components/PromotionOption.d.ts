d594de9d8d8125b25652e307748d6353f8b834a8
Matt Pat <matteo.patella@studio.unibo.it>
2023-11-07 13:17:13 +0100

    fixed bug: node_modules downloaded on main branch

--- code/omega-chess/node_modules/react-chessboard/dist/chessboard/components/PromotionOption.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { PromotionPieceOption } from "../types";
-type Props = {
-    option: PromotionPieceOption;
-};
-export declare function PromotionOption({ option }: Props): JSX.Element;
-export {};
