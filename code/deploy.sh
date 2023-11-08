52d7e73e887e077396c73d219ce630ccc6ddfbb3
gianlourbano <gianlorenzourbano@gmail.com>
2023-11-08 19:36:29 +0100

    adds containers healthcheck

--- code/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script is used to deploy the Omega Chess application to the server
-
-rsync -azP --exclude={".next","node_modules"} $(pwd)/* gianlorenzo@pi:~/OMEGA
-
-ssh gianlorenzo@pi 'cd ~/OMEGA && docker compose down && docker compose build --no-cache && docker compose up -d'
--- code/telegram-bot-server/index.js
+++ code/telegram-bot-server/index.js
@@ -75,6 +75,9 @@ bot.action('newgame', (ctx) => {
     ctx.reply(`New game starting at ${process.env.WEB_APP_URI}}`)
 })
 
+app.get("/health", (req, res) => {
+    res.send("OK");
+});
 
 app.listen(process.env.PORT || 4000, () => {
     console.log(`Server is listening on port ${process.env.PORT || 4000}`);
