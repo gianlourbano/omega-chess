8439faf0be6aa826b0275e57edbdbe5db7f11b38
gianlourbano <gianlorenzourbano@gmail.com>
2023-12-05 18:36:40 +0100

    fixes

--- code/omega-chess/src/components/QuickRules.tsx
+++ code/omega-chess/src/components/QuickRules.tsx
@@ -11,36 +11,36 @@ function QuickRules() {
             <div className="rounded-lg bg-zinc-600 mr-2">
                 <h1 className="text-xl font-bold text-center"> Separazione dei Giocatori </h1>
                 <ul className="list-disc list-inside pl-2 justify-evenly">
-                    <li className="text-md"> Ogni giocatore ha una scacchiera separata senza vedere le mosse dell'avversario. </li>
-                    <li className="text-md"> L'arbitro gestisce una terza scacchiera con la posizione completa. </li>
+                    <li className="text-md"> Ogni giocatore ha una scacchiera separata senza vedere le mosse dell&aposavversario. </li>
+                    <li className="text-md"> L&aposarbitro gestisce una terza scacchiera con la posizione completa. </li>
                 </ul>
             </div>
             <div className="rounded-lg bg-zinc-600 mr-2">
                 <h1 className="text-xl font-bold text-center"> Arbitro </h1>
                 <ul className="list-disc list-inside pl-2 justify-evenly">
-                    <li className="text-md"> Comunica con i giocatori per annunciare l'esito delle mosse. </li>
-                    <li className="text-md"> Annuncia il cambio di turno se la mossa è legale, altrimenti dichiara "mossa illegale". </li>
+                    <li className="text-md"> Comunica con i giocatori per annunciare l&aposesito delle mosse. </li>
+                    <li className="text-md"> Annuncia il cambio di turno se la mossa è legale, altrimenti dichiara &#34mossa illegale&#34. </li>
                 </ul>
             </div>
             <div className="rounded-lg bg-zinc-600 mr-2">
                 <h1 className="text-xl font-bold text-center"> Scacco e Cattura </h1>
                 <ul className="list-disc list-inside pl-2 justify-evenly">
-                    <li className="text-md"> L'arbitro annuncia uno scacco, specificando la direzione. </li>
+                    <li className="text-md"> L&aposarbitro annuncia uno scacco, specificando la direzione. </li>
                     <li className="text-md"> Catture annunciate in termini generali (cattura in [casa]), en-passant non specificato. </li>
                 </ul>
             </div>
             <div className="rounded-lg bg-zinc-600 mr-2">
                 <h1 className="text-xl font-bold text-center"> Catture di Pedoni e Promozione </h1>
                 <ul className="list-disc list-inside pl-2 justify-evenly">
-                    <li className="text-md"> Prima di ogni mossa, l'arbitro comunica il numero di catture di pedone possibili. </li>
+                    <li className="text-md"> Prima di ogni mossa, l&aposarbitro comunica il numero di catture di pedone possibili. </li>
                     <li className="text-md"> Promozioni del pedone non annunciate, lasciate ai giocatori per deduzione. </li>
                 </ul>
             </div>
             <div className="rounded-lg bg-zinc-600 mr-2">
                 <h1 className="text-xl font-bold text-center"> Scacco Matto e Patta </h1>
                 <ul className="list-disc list-inside pl-2 justify-evenly">
-                    <li className="text-md"> L'arbitro annuncia scacco matto, terminando la partita. </li>
-                    <li className="text-md"> Condizioni di patta seguono le regole standard, con l'eccezione della patta per impossibilità di dare scacco matto </li>
+                    <li className="text-md"> L&aposarbitro annuncia scacco matto, terminando la partita. </li>
+                    <li className="text-md"> Condizioni di patta seguono le regole standard, con l&aposeccezione della patta per impossibilità di dare scacco matto </li>
                     <li className="text-md"> La regola delle 50 mosse è in vigore. I giocatori devono tenere traccia delle mosse per richiedere la patta in base a questa regola. </li>
                 </ul>
             </div>
